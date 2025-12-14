eec9611b7732fa2eee0b5dc5ffe87a9fbac10c50
shime <79533618+shime69@users.noreply.github.com>
2025-12-14 20:01:13 +0100

    HomePage User
    Umesto starog Alex username, pise sad vas username sa baze.

--- MindwavE/HomePage.xaml.cs
+++ MindwavE/HomePage.xaml.cs
@@ -2,10 +2,32 @@ namespace MindwavE;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
+    private readonly Services.AuthService _authService;
+
+	public HomePage(Services.AuthService authService)
 	{
 		InitializeComponent();
+        _authService = authService;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        UpdateWelcomeMessage();
+    }
+
+    private void UpdateWelcomeMessage()
+    {
+        var currentUser = _authService.GetCurrentUser();
+        if (currentUser != null && currentUser.UserMetadata.TryGetValue("username", out var usernameObj))
+        {
+            string username = usernameObj?.ToString();
+            if (!string.IsNullOrEmpty(username))
+            {
+                WelcomeLabel.Text = $"Dobrodošao/la nazad, {username}!";
+            }
+        }
+    }
 
     private async void GoToChatPage(object sender, EventArgs e)
     {
